0f5aa74a77cc7af71a68226eb7cca413cc122af8
sjmoon <moonsun116@naver.com>
2025-01-09 10:07:49 +0900

    [feat] 로그인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Nav from "@/components/Nav";
 import styles from "./page.module.scss";
+import Login from "@/components/Login";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Nav />
+      <Login />
+      {/* <Nav /> */}
     </div>
   );
 }
