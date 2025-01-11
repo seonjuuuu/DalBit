a88e221c9577deb540a2d77edb9bf52bbfd00f31
moonseonju <sjmoon@glowmee.com>
2025-01-12 08:56:20 +0900

    [feat] 체크박스 설정

--- src/components/Hongkong/HongKongTable.tsx
+++ src/components/Hongkong/HongKongTable.tsx
@@ -1,9 +1,49 @@
 "use client";
+import { useState } from "react";
 import ToggleButton from "../common/ToggleButton";
 import styles from "./HongKongTable.module.scss";
-import Switch from "react-switch";
 
 const HongKongTable = () => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
+
+  const data = [
+    {
+      id: 1,
+      title: "번역작업",
+      category: "번역",
+      date: "2024-01-01",
+      amount: "500,000",
+      settleDate: "2024-01-27"
+    },
+    {
+      id: 2,
+      title: "아웃도어캠페인",
+      category: "홈페이지",
+      date: "2024-01-01",
+      amount: "500,000",
+      settleDate: "2024-01-27"
+    }
+  ];
+
+  const handleSelectAll = () => {
+    const newCheckedState = !isChecked;
+    setIsChecked(newCheckedState);
+    setCheckedItems(new Array(data.length).fill(newCheckedState));
+  };
+
+  const handleCheckboxChange = (index: number) => {
+    const newCheckedItems = [...checkedItems];
+    newCheckedItems[index] = !newCheckedItems[index];
+    setCheckedItems(newCheckedItems);
+
+    if (newCheckedItems.every((item) => item)) {
+      setIsChecked(true);
+    } else {
+      setIsChecked(false);
+    }
+  };
+
   return (
     <div className={styles.mainTable}>
       <div className={styles.cost}>
@@ -16,32 +56,43 @@ const HongKongTable = () => {
       <table className="tableList">
         <thead>
           <tr>
+            <th>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleSelectAll}
+              />
+            </th>
             <th>번호</th>
             <th>제목</th>
             <th>분류</th>
+            <th>작업날짜</th>
             <th>금액</th>
             <th>정산여부</th>
+            <th>정산날짜</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>번역작업</td>
-            <td>번역</td>
-            <td>500,000</td>
-            <td>
-              <ToggleButton />
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>번역작업</td>
-            <td>번역</td>
-            <td>500,000</td>
-            <td>
-              <ToggleButton />
-            </td>
-          </tr>
+          {data.map((item, index) => (
+            <tr key={item.id}>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={checkedItems[index]}
+                  onChange={() => handleCheckboxChange(index)}
+                />
+              </td>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.category}</td>
+              <td>{item.date}</td>
+              <td>{item.amount}</td>
+              <td>
+                <ToggleButton />
+              </td>
+              <td>{item.settleDate}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
