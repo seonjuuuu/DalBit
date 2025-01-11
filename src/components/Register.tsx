8c5e292fdf766e9ad8d02793c483fc2b6aecdcd4
moonseonju <sjmoon@glowmee.com>
2025-01-11 22:36:31 +0900

    [feat] 회원가입 ui 작업

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,8 +2,30 @@ import styles from "./Register.module.scss";
 
 const Register = () => {
   return (
-    <div>
-      <div>회원가입</div>
+    <div className={styles.register}>
+      <h2 className={styles.title}>회원가입</h2>
+      <div className={styles.inputWrap}>
+        <label className={styles.inputBox}>
+          <span>아이디</span>
+          <input type="text" />
+        </label>
+        <label className={styles.inputBox}>
+          <span>닉네임</span>
+          <input type="text" />
+        </label>
+        <label className={styles.inputBox}>
+          <span>비밀번호</span>
+          <input type="password" />
+        </label>
+        <label className={styles.inputBox}>
+          <span>비밀번호 확인</span>
+          <input type="password" />
+        </label>
+      </div>
+      <div className={styles.btnWrap}>
+        <button className={styles.registerBtn}>회원가입</button>
+        <button className={styles.loginBtn}>로그인하기</button>
+      </div>
     </div>
   );
 };
