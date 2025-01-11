1d6bace04f655da3fd514317938396a9f42fbecb
moonseonju <sjmoon@glowmee.com>
2025-01-11 22:42:34 +0900

    [feat] register 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Nav from "@/components/Nav";
 import styles from "./page.module.scss";
+import HongKongRegister from "@/components/Hongkong/HongKongRegister";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Nav />
+      <main className={styles.main}>
+        <HongKongRegister />
+      </main>
     </div>
   );
 }
---
+++ src/components/Hongkong/HongKongRegister.tsx
@@ -0,0 +1,11 @@
+import styles from "./HongKongRegister.module.scss";
+
+const HongKongRegister = () => {
+  return (
+    <div>
+      <div>홍콩</div>
+    </div>
+  );
+};
+
+export default HongKongRegister;
