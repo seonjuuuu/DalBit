056637cfd244d341f0e7db8feb3800233df487d9
moonseonju <sjmoon@glowmee.com>
2025-01-13 16:14:50 +0900

    [feat] 로그아웃 기능 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import styles from "./Nav.module.scss";
+import Cookies from "js-cookie";
 
 const Nav = () => {
   const router = useRouter();
@@ -13,17 +14,27 @@ const Nav = () => {
     router.push("/");
   };
 
+  const handleLogout = () => {
+    Cookies.remove("jwtToken");
+    router.push("/login");
+  };
+
   return (
     <nav className={styles.nav}>
-      <img
-        className={styles.logo}
-        src="/dalbit-kr.png"
-        alt="logo"
-        onClick={handleLogo}
-      />
-      <ul className={styles.navList}>
-        <li onClick={handleHongKong}>홍콩관광청</li>
-      </ul>
+      <div className={styles.navWrap}>
+        <img
+          className={styles.logo}
+          src="/dalbit-kr.png"
+          alt="logo"
+          onClick={handleLogo}
+        />
+        <ul className={styles.navList}>
+          <li onClick={handleHongKong}>홍콩관광청</li>
+        </ul>
+      </div>
+      <button className={styles.logout} onClick={handleLogout}>
+        로그아웃
+      </button>
     </nav>
   );
 };
